b476e3116e803a609483b64529cec54b7bb22391
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-09-13 12:08:23 +0500

    Update the MainActivity Added Field Username and Button Login Through Java Code

--- app/src/main/java/com/example/usmankhan/termproject/MainActivity.java
+++ app/src/main/java/com/example/usmankhan/termproject/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.usmankhan.termproject;
 
+import android.content.res.Resources;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.TypedValue;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,8 +18,53 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
         Log.i(TAG,"onCreate");
+
+        RelativeLayout mulayout=new RelativeLayout(this);
+        Button btn=new Button(this);
+
+
+
+        mulayout.setBackgroundColor(Color.MAGENTA);
+        btn.setBackgroundColor(Color.BLUE);
+        btn.setText("Click Me");
+        btn.setTextColor(Color.WHITE);
+
+        RelativeLayout.LayoutParams btnDetails= new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT
+        );
+        btnDetails.addRule(RelativeLayout.CENTER_HORIZONTAL);
+        btnDetails.addRule(RelativeLayout.CENTER_VERTICAL);
+
+        EditText username=new EditText(this);
+        username.setTextColor(Color.WHITE);
+        username.setHint("Enter UserName");
+        username.setHintTextColor(Color.WHITE);
+        btn.setId(1);
+        username.setId(2);
+
+        RelativeLayout.LayoutParams usernameDetails=new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT
+        );
+        usernameDetails.addRule(RelativeLayout.ABOVE,btn.getId());
+        usernameDetails.addRule(RelativeLayout.CENTER_HORIZONTAL);
+        usernameDetails.setMargins(0,0,0,50);
+        Resources r=getResources();
+        int pixel=(int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,300,r.getDisplayMetrics());
+        username.setWidth(pixel);
+
+        EditText password=new EditText(this);
+        password.setTextColor(Color.WHITE);
+
+
+
+        mulayout.addView(username,usernameDetails);
+        mulayout.addView(btn,btnDetails);
+        setContentView(mulayout);
+
     }
 
     @Override
