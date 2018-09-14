3b89344adc2b947992d8057560c344cf274aab2f
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-09-14 09:18:41 +0500

    Update the EventActivity Added Listners to Perform the Actions

--- app/src/main/java/com/example/usmankhan/termproject/EventActivity.java
+++ app/src/main/java/com/example/usmankhan/termproject/EventActivity.java
@@ -3,6 +3,9 @@ package com.example.usmankhan.termproject;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 
 public class EventActivity extends AppCompatActivity {
@@ -11,5 +14,17 @@ public class EventActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event);
+
+
+        Button btn_click=(Button) findViewById(R.id.btn_click);
+        btn_click.setOnClickListener(
+                new Button.OnClickListener(){
+                    public void onClick(View view)
+                    {
+                        TextView text_hello=(TextView) findViewById(R.id.text_hello);
+                        text_hello.setText("Clicked By Usman");
+                    }
+                }
+        );
     }
 }
